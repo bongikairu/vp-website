923bfc24bc45be3a18171d2e7675020f3d18fa8f
BongiKairu <bongikairu@gmail.com>
2012-06-21 11:03:54 +0700

    First version of index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Vintage Penguin Interactive</title>
+		<link href='http://fonts.googleapis.com/css?family=Karla:400,700,400italic' rel='stylesheet' type='text/css'>
+		<style type="text/css">
+			body {
+				font-family: 'Karla', sans-serif;
+			}
+			#pagewrapper {
+				width: 700px;
+				margin: 200px auto;
+			}
+			#example {
+				font-style: italic;
+			}
+			#example .vp {
+				font-weight: bold;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="pagewrapper">
+			<h1>vintage /ˈvɪn.tɪdʒ/</h1>
+			<p>(adj) from the time that is glorious, of great quality with<br>memories, of retrospect fashion wile proper to the present:</p>
+			<p id="example"><span class="vp">Vintage Penguin Interactive</span> is a company specialized in making the best application.</p>
+		</div>
+	</body>
+</html>
