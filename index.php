ea037d75eb23d1240fe84056696ce0934825d5fa
BongiKairu <bongikairu@gmail.com>
2012-06-21 15:04:55 +0700

    rework on font, with fallback to times -- also add shadow to make it look emboss coloring background, need to have some wallpaper pasted instead add menu

--- index.php
+++ index.php
@@ -3,28 +3,35 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Vintage Penguin Interactive</title>
-		<link href='http://fonts.googleapis.com/css?family=Karla:400,700,400italic' rel='stylesheet' type='text/css'>
-		<style type="text/css">
-			body {
-				font-family: 'Karla', sans-serif;
-			}
-			#pagewrapper {
-				width: 700px;
-				margin: 200px auto;
-			}
-			#example {
-				font-style: italic;
-			}
-			#example .vp {
-				font-weight: bold;
-			}
-		</style>
+		
+		<!-- Blueprint -->
+		<link rel="stylesheet" href="assets/blueprint/screen.css" type="text/css" media="screen, projection">
+		<link rel="stylesheet" href="assets/blueprint/print.css" type="text/css" media="print">
+		<!--[if lt IE 8]><link rel="stylesheet" href="assets/blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
+		
+		<link href='assets/style.css' rel='stylesheet' type='text/css'>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
 	</head>
 	<body>
-		<div id="pagewrapper">
-			<h1>vintage /ˈvɪn.tɪdʒ/</h1>
-			<p>(adj) from the time that is glorious, of great quality with<br>memories, of retrospect fashion wile proper to the present:</p>
-			<p id="example"><span class="vp">Vintage Penguin Interactive</span> is a company specialized in making the best application.</p>
+		<div id="pagewrapper" class="container">
+			<div id="leftcol" class="span-16 append-2">
+				&nbsp;
+			</div>
+			<div id="rcol-about" class="span-14 last">
+				<h1 id="word">vintage <span class="sound">/ˈvɪn.tɪdʒ/</span></h1>
+				<p>(adj) from the time that is glorious, of great quality with memories, of retrospect fashion while proper to the present:</p>
+				<p id="example"><span class="vp">Vintage Penguin Interactive</span> is a company specialized in making the best application.</p>
+			</div>
+			<div id="menu" class="span-14 last">
+				<ul>
+					<li><a href="index.php" class="active">About Us</a></li>
+					<li><a href="index.php">Products</a></li>
+					<li><a href="index.php">News</a></li>
+					<li><a href="index.php">Dev Blog</a></li>
+					<li><a href="index.php">Contact Us</a></li>
+				</ul>
+			</div>
 		</div>
 	</body>
 </html>
